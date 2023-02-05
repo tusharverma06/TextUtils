90b74c43568754ed4505660a26460781652c3b4c
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-02-05 16:53:58 +0530

    Update navbar.jsx

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -7,11 +7,9 @@ export default function ExampleNav(props){
         <nav className={`navbar navbar-expand-lg bg-${props.mode}`}>
   <div className="container-fluid">
     <Link className={`navbar-brand ${props.mode==='dark'? 'text-light': 'text-dark'}`} to="/">{props.title}</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
+    
+    <div id="navbarNav" style={{width: '200px'}}>
+      <ul className="navItems">
         <li className="nav-item">
           <Link className={`nav-link active ${props.mode==='dark'? 'text-light': 'text-dark'}`} aria-current="page" to="/">Home</Link>
         </li>
