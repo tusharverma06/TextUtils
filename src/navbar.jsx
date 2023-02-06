3174a89b73ae937ca98328eccaef3f33907ba307
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-02-06 16:05:51 +0530

    Update navbar.jsx
    added pointer cursor to dark and light mode images

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -26,9 +26,9 @@ export default function ExampleNav(props){
 
   </div>
   <div className="modeSwitch">
-<div className="form-check form-switch mx-3" onClick={props.toggleMode}>
-  {props.mode==='light'?   <img src="https://cdn2.iconfinder.com/data/icons/user-interface-vol-2-14/48/brightness-light-up-day-mode-512.png" alt="" width={'30px'} />
-:<img src={DarkMode} alt='' width={'30px'}/>
+<div className="form-check form-switch mx-3" onClick={props.toggleMode} >
+  {props.mode==='light'?   <img src="https://cdn2.iconfinder.com/data/icons/user-interface-vol-2-14/48/brightness-light-up-day-mode-512.png" alt="" width={'30px'} style={{cursor: 'pointer'}}/>
+:<img src={DarkMode} alt='' width={'30px'} style={{cursor: 'pointer'}}/>
 }
 
 </div>
